d2b37d7181ac6bd2a454ae9cb807a09cab4f667b
Brian Furman <bfurman777@yahoo.com>
2020-03-09 20:26:11 -0500

    full cisapi parse into JSON

---
+++ data_retrieval/README.md
@@ -0,0 +1,7 @@
+
+run with `python3 fetch_cisapi.py`
+may take a while to run (more than 10 min)
+
+result data run for Spring 2020 semester (3/9/20)
+https://drive.google.com/drive/folders/1fRbb8RN9YnsrNGM5G_C8wmH1Zw4JDiF6?usp=sharing
+
--- data_retrieval/fetch_cisapi.py
+++ data_retrieval/fetch_cisapi.py
@@ -1,65 +1,185 @@
 
 import requests 
-from bs4 import BeautifulSoup  # pip install bs4 && python3 -m pip install lxml 
+from bs4 import BeautifulSoup
+import re
+import json
 
+# set to None if you want to process all courses, otherwise provide list of Subjects
+RESTRICT_TO_SUBJECTS = None  
+# RESTRICT_TO_SUBJECTS = ['AAS']
+
+LOG_FILE = None
+
+# Parse data from cisapi, then write it in JSON format:
+example = { 
+    'courses': [
+        { 
+            'course_num': '225',
+            'department': 'CS',
+            'prereqs': ['String with "Prerequisite:" in it'],  # TODO, see parse_requirements()
+            'sections': [
+                {
+                    'section': 'ADE',
+                    'crn': '618201',
+                    'meetings': [
+                        {
+                            'building': 'Siebel',
+                            'room': '234',
+                            'start': '9:00am',
+                            'end': '9:50am'
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
+
+'''
+initialize regex compiled objects, etc
+'''
+def init():
+    global LOG_FILE
+    LOG_FILE = open('log.txt', 'w')
+
+
+'''
+log the error and print it to terminal
+'''
+def log_error(err_string):
+    print(err_string + '\n')
+    LOG_FILE.write(err_string + '\n')
+
+'''
+given the string that follows "Requirements: ", extract it into a "prereqs" array
+TODO: group them by requirement groups
+    ex. 'CS 125 or ECE 220; One of CS 173, MATH 213'
+    becomes a list of class groups:
+    { 'prereqs': [['CS 125', 'ECE 220'], ['CS 173', 'MATH 213']] }
+TODO: handle concurrent classes?
+'''
+def parse_requirements(requirements_string):
+    if requirements_string is None:
+        return None
+    return [requirements_string] # TODO impleament with regex
+
+
+'''
+request the XML file from the link, then soupify it
+'''
 def link_to_soup(link):
-    r = requests.get(link).text
-    return BeautifulSoup(r, 'lxml')  # 'html.parser' instead of 'lxml'
+    try:
+        r = requests.get(link).text
+        return BeautifulSoup(r, 'html.parser')
+    except Exception as e:
+        log_error(f'ERROR: Could not obtain \n{link}')
+        return None
 
+
+'''
+parse all class data from the cisapi, and writes to a json:
+'''
 def parse_semester(year, sem, json_out):  #2020, spring
+    init()
     link = f'https://courses.illinois.edu/cisapp/explorer/schedule/{year}/{sem.lower()}.xml'
     soup = link_to_soup(link)
+    courses = []
     for subject in soup.find_all('subject'):
-        try:
-            print(subject['id'])
-            parse_subject(subject['href'])
-        except:
-            print("ERROR: could not parse:\n\t", subject)
-    pass
+        if RESTRICT_TO_SUBJECTS is None or (RESTRICT_TO_SUBJECTS is not None and subject['id'] in RESTRICT_TO_SUBJECTS):
+            print('Subject: ', subject['id'])
+            parse_subject(subject['href'], courses)
+    with open(json_out, 'w') as fd:
+        fd.write(json.dumps({'courses': courses}))
 
-def parse_subject(link):
-    soup = link_to_soup(link)
-    for course in soup.find_all('course'):
-        try:
-            print(course['id'])
-            parse_class(course['href'])
-        except:
-            print("ERROR: could not parse:\n\t", course)
 
-def parse_class(link):
+'''
+appends all courses from a subject (as dictionaries) to the given "courses" list
+'''
+def parse_subject(link, courses):
     soup = link_to_soup(link)
+    try:
+        for course in soup.find_all('course'):
+            courses.append(parse_course(course['href']))
+    except:
+        log_error(f'SKIPPING\n{link}')
+
+
+'''
+return a "course" dictionary that also contains a list of "section" dictionaries
+'''
+def parse_course(link):
+    course_dict = { 'course_num': None, 'department': None, 'prereqs': [], 'sections': [] }
+    soup = link_to_soup(link)
+    try:
+        id = soup.find('ns2:course')['id'].split(' ')
+        course_dict['department'], course_dict['course_num'] = id
+    except:
+        log_error(f'ERROR: \n{link}\n has no department or course number!')
+    
+    match = re.match(r'.*Prerequisite: (.*?)\.', str(soup))
+    if match is not None:
+        course_dict['prereqs'] = parse_requirements(match.group(1))
     for section in soup.find_all('section'):
-        try:
-            print(section.text)
-            parse_section(section['href'])
-        except:
-            print("ERROR: could not parse:\n\t", section)
+        course_dict['sections'].append(parse_section(section['href']))
 
+    # checking edge cases for Prerequisite -> notify us in terminal
+    if match is None:
+        match = re.match(r'.*Prerequisite.*', str(soup), re.IGNORECASE)  
+        if match is not None:
+            log_error(f'NOTE: \n{link}\n has "Prerequisite" without a period or different case')
+        match = re.match(r'.*concurrent.*', str(soup), re.IGNORECASE)
+        if match is not None:
+            log_error(f'NOTE: \n{link}\n has "concurrent" without a Prerequisite')
+    
+    return course_dict
+
+
+'''
+return a "section" dictionary that also contains a list of "meeting" dictionaries
+'''
 def parse_section(link):
+    section_dict = { 'section': None, 'crn': None, 'meetings': [] }
     soup = link_to_soup(link)
+    try:
+        section_dict['crn'] = soup.find('ns2:section')['id']
+    except:
+        error_text = f'ERROR: \n{link}\n has no crn!'
+        log_error(f'ERROR: \n{link}\n has no crn!')
+        LOG_FILE.write(f'ERROR: \n{link}\n has no crn!')
+    if soup.sectionnumber is not None:
+        section_dict['section'] = soup.sectionnumber.text
     for meeting in soup.find_all('meeting'):
+        meeting_dict = { 'building': None, 'room': None, 'start': None, 'end': None }
         if meeting.buildingname is not None:
-            print(meeting.buildingname.text, meeting.roomnumber.text)
-            print(meeting.start.text, "->", meeting.end.text)
-        else:
-            print('Online')
+            meeting_dict['building'] = meeting.buildingname.text
+        if meeting.roomnumber is not None:
+            meeting_dict['room'] = meeting.roomnumber.text
+        if meeting.start is not None:
+            meeting_dict['start'] = meeting.start.text
+        if meeting.end is not None:
+            meeting_dict['end'] = meeting.end.text
+        section_dict['meetings'].append(meeting_dict)
+    return section_dict
 
 if __name__ == '__main__':
-    # TODO revert to friendly user input
-    # year = input('Enter year: ')
-    # sem = input('Enter semester (fall/spring): ')
-    # parse_semester(year, sem, None)
-    parse_semester(2020, 'spring', None)
+    parse_semester(2020, 'spring', 'cisdata.json')
+
+    # testing
+    # ret = parse_section('https://courses.illinois.edu/cisapp/explorer/schedule/2020/spring/AAS/100/30107.xml')
+    # ret = parse_course('https://courses.illinois.edu/cisapp/explorer/schedule/2020/spring/PHYS/213.xml')
+    
+    
+    # parse_subject('https://courses.illinois.edu/cisapp/explorer/schedule/2020/spring/AAS.xml')
 
 ''' 
-    TODO
-nullchecks
+    TODO maybe
+cache regex compiled objects for speed
 start/end dates in parse_section (for half-sem courses)?
-actual XML parser or is HTML ok?
-list of meetings
+
+    NOTE
+soup lowercases all tags
+should we use the actual XML parser or is soup HTML ok?
+meetings is a list - CS 233 has 1 section that meets twice in different rooms
 '''
 
-# testing
-# parse_section('https://courses.illinois.edu/cisapp/explorer/schedule/2020/spring/AAS/100/30107.xml')
-# parse_class('https://courses.illinois.edu/cisapp/explorer/schedule/2020/spring/AAS/100.xml')
-# parse_subject('https://courses.illinois.edu/cisapp/explorer/schedule/2020/spring/AAS.xml')
+
