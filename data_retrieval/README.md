0cf6cc9f8dbf1367771fb87781e2185b1efdc71b
Brian Furman <bfurman777@yahoo.com>
2020-03-24 20:46:17 -0500

    neo4j interface works for classes

--- data_retrieval/README.md
+++ data_retrieval/README.md
@@ -5,3 +5,22 @@ may take a while to run (more than 10 min)
 result data run for Spring 2020 semester (3/9/20)
 https://drive.google.com/drive/folders/1fRbb8RN9YnsrNGM5G_C8wmH1Zw4JDiF6?usp=sharing
 
+Neo4j:
+
+- Nodes
+  - (:Course { dept: "CS", num: 411 } )	
+  - (:Section { crn: 618234 } )
+  - (:Meeting { room: 214, start: "9:00am", end: "9:50am" } )
+  - (:Building { name: "Siebel Center" } )
+- Relations
+  - (:Section)-[:SectionOf]->(:Course)
+  - (:Meeting)-[:MeetsFor]->(:Section)
+  - (:Meeting)-[:LocatedAt]->(:Building)
+
+TODO: 
+
+- prereqs!
+- parse time into date object/int?
+- getters for python interface
+- remove "Virtual" meetings from the database
+- remove "MERGE" in query for speed?
--- data_retrieval/neo4j_interface.py
+++ data_retrieval/neo4j_interface.py
@@ -1,6 +1,9 @@
 
 from neo4j import GraphDatabase
 
+URI = 'bolt://localhost:7687'
+USER = 'neo4j'
+PW = 'password'
 
 class Neo4j_Interface():
 
@@ -10,43 +13,83 @@ class Neo4j_Interface():
     def close(self):
         self._driver.close()
 
-    def print_greeting(self, message):
+    def delete_all(self):
         with self._driver.session() as session:
-            greeting = session.write_transaction(self._methods._create_and_return_greeting, message)
-            print(greeting)
+            session.write_transaction(self._methods._delete_all)
 
-    def delete_all_nodes(self):
+    def count_nodes(self):
         with self._driver.session() as session:
-            session.write_transaction(self._methods._delete_all_nodes)
+            session.write_transaction(self._methods._count_nodes)
 
-    def select_all_nodes(self):
+    def add_course(self, dept, num):
         with self._driver.session() as session:
-            session.write_transaction(self._methods._select_all_nodes)
+            session.write_transaction(self._methods._add_course, dept, num)
+
+    def add_section(self, dept, num, crn):
+        with self._driver.session() as session:
+            session.write_transaction(self._methods._add_section, dept, num, crn)
+
+    def add_meeting(self, crn, start, end, building, room):
+        with self._driver.session() as session:
+            session.write_transaction(self._methods._add_meeting, crn, start, end, building, room)
+
+    # TODO:
+    # course -> get info
+    # course -> get sections
+    # crn (section) -> get meetings
 
     class _methods():
         
         @staticmethod
-        def _delete_all_nodes(tx):
-            tx.run("MATCH (a)"
-                    "DELETE (a)")         
+        def _delete_all(tx):
+            tx.run(
+                "MATCH (a) "
+                "DETACH DELETE a ")         
 
         @staticmethod
-        def _create_and_return_greeting(tx, message):
-            result = tx.run("CREATE (a:Greeting {message: $message}) "
-                            "RETURN a.message", 
-                            message=message)
-            return result.single()[0]
-
-        @staticmethod
-        def _select_all_nodes(tx):
+        def _count_nodes(tx):
             result = tx.run("MATCH (a) "
                             "RETURN a")
             print(f'num of nodes: {len([i for i in result.records()])}')
 
+        @staticmethod
+        def _add_course(tx, dept, num):
+            tx.run(
+                "CREATE (c:Course { dept: $dept, num: $num }) ",
+                dept=dept, num=num)
+
+        @staticmethod
+        def _add_section(tx, dept, num, crn):
+            tx.run(
+                "MATCH (c:Course) WHERE c.dept = $dept AND c.num = $num "
+                "CREATE (s:Section { crn: $crn }) "
+                "CREATE (s)-[:SectionOf]->(c)",
+                dept=dept, num=num, crn=crn)
+
+        @staticmethod
+        def _add_meeting(tx, crn, start, end, building, room):
+            tx.run(
+                "MATCH (s:Section) WHERE s.crn = $crn "
+                "CREATE (m:Meeting { room: $room, start: $start, end: $end }) "
+                "MERGE (b:Building { name: $building }) "
+                "CREATE (m)-[:MeetsFor]->(s) "
+                "CREATE (m)-[:locatedAt]->(b)",
+                crn=crn, room=room, start=start, end=end, building=building)
+
 
 if __name__ == "__main__":
-    db = Neo4j_Interface('bolt://localhost:7687', 'neo4j', 'password')
-    db.delete_all_nodes()
-    db.print_greeting("hellow!")
-    db.print_greeting("hellow!")
-    db.select_all_nodes()
+    db = Neo4j_Interface(URI, USER, PW)
+
+    db.delete_all()
+    # db.add_course("CS", 225)
+
+    # db.add_course("CS", 411)
+    # db.add_section("CS", 411, 61802)
+
+    # db.add_meeting(61802, "9:00pm", "11:00pm", "Siebel", 214)
+    # db.add_meeting(61802, "8:00pm", "9:00pm", "ECEB", 214)
+
+    # db.add_section("CS", 411, 61869)
+    # db.add_meeting(61869, "1:00pm", "2:00pm", "Siebel", 214)
+
+    db.count_nodes()
