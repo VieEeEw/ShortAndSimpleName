eb27f7fe141dc0af285d0752c6da898afb8f4470
VieEeEw <786851669@qq.com>
2020-03-26 18:30:30 -0500

    Merge from master branch. Modify schema.sql

--- server/schema.sql
+++ server/schema.sql
@@ -2,6 +2,6 @@ DROP TABLE IF EXISTS user;
 
 CREATE TABLE user (
     `net_id`    VARCHAR(10)     PRIMARY KEY,
-    `name`      VARCHAR(255)    NOT NULL,
+    `name`      VARCHAR(255),
     `pswd`      VARCHAR(255)    NOT NULL
 );
