f2ff9c96ff653db8fcb3daefbcf295fcbb8f6a56
VieEeEw <786851669@qq.com>
2020-04-10 16:05:42 -0500

    Change database settings. Adding endpoints

--- server/auth.py
+++ server/auth.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 
-from flask import Blueprint, make_response, request
+from flask import Blueprint, make_response, request, session
 from werkzeug.security import check_password_hash, generate_password_hash
+from hashlib import md5
 
 from .db import get_db
 
@@ -22,8 +23,10 @@ def login():
         else:
             # If the token is invalid, generate a new token for the user
             if user['xpire_t'] < datetime.utcnow():
-                db.execute("UPDATE user SET token = '[new_token]' WHERE net_id = ?",
-                           (netid,))  # FIXME Change new_token to a valid new token
+                new_token = md5(f"{password}{datetime.utcnow()}".encode()).hexdigest()
+                db.execute("UPDATE user SET token = ? WHERE net_id = ?",
+                           (new_token, netid))
+                session['token'] = new_token
             return make_response({
                 'status': "Login successfully"
             }, 200)
@@ -43,11 +46,31 @@ def register():
         elif db.execute('SELECT * FROM user WHERE net_id = ?', (netid,)).fetchone() is not None:
             error = f'Netid {netid} already registered, try login instead.'
         else:
-            db.execute('INSERT INTO user(net_id, name, pswd, token) VALUES (?, ?, ?, "[new_token]")',
+            new_token = md5(f"{password}{datetime.utcnow()}".encode()).hexdigest()
+            db.execute('INSERT INTO user(net_id, name, pswd, token) VALUES (?, ?, ?, ?)',
                        (netid, request.json['name'] if 'name' in request.json else None,
-                        generate_password_hash(password)))  # FIXME Change new_token to a valid new token
+                        generate_password_hash(password)), new_token)
             db.commit()
             return make_response({'status': "create successfully"}, 200)
         return make_response({
             'error': error
         }, 403)
+
+
+@bp.route('/delete', method=['POST'])
+def delete():
+    netid = request.json['net_id']
+    db = get_db()
+    token, e_time = db.execute("SELECT token, xpire_t FROM user WHERE net_id=?", (netid,)).fetchone()
+    if token is None or e_time > datetime.utcnow():
+        error = 'Token expired.'
+    elif token != session['token']:
+        error = 'Invalid token.'
+    else:
+        db.execute("DELETE FROM user WHERE net_id=?", (netid,))
+        db.commit()
+        session.pop('token')
+        return make_response({'status': "delete successfully"}, 200)
+    return make_response({
+        'error': error
+    }, 403)
---
+++ server/data.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, make_response, request, session
+from .db import get_db, get_graph_db
+
+bp = Blueprint('data', __name__, url_prefix='/data')
+
+
+@bp.route('/course/<subject>/<number>', method=['GET', 'PUT', 'PATCH', 'DELETE'])
+def course_dispatch(subject, number):
+    # FIXME check token and net_id
+    gdb = get_graph_db()
+    if request.method == 'GET':
+        pass
+    elif request.method == 'PUT':
+        pass
+    elif request.method == 'PATCH':
+        pass
+    elif request.method == 'DELETE':
+        pass
+
+
+@bp.route('/courses', method=['GET'])
+def courses():
+    return get_graph_db().get_all_courses()
