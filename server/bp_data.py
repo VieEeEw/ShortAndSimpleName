b1be41afc8ed74f2e52c28596aceabfb59704b52
VieEeEw <786851669@qq.com>
2020-05-06 14:02:50 -0500

    Return a response for GET in /data/crn

--- server/bp_data.py
+++ server/bp_data.py
@@ -40,7 +40,8 @@ def crn():
     if code != 200:
         return make_response({'error': msg}, code)
     if request.method == 'GET':
-        return rdb.execute("SELECT `crn` FROM `user_crn` WHERE net_id=?", (netid,)).fetchall()
+        return make_response({'crns': rdb.execute("SELECT `crn` FROM `user_crn` WHERE net_id=?", (netid,)).fetchall()},
+                             200)
     elif request.method == 'POST':
         ls = rdb.execute("SELECT `crn` FROM `user_crn` WHERE net_id=?", (netid,)).fetchall()
         for CRN in request.json['crns']:
