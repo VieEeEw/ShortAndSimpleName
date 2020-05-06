6a06d12ae5f5d2c9c46e0663d56da150283b0c91
Yifan Ye <yeyifan9@gmail.com>
2020-05-06 06:47:56 -0500

    sidebar

--- frontend/src/pages/Index.vue
+++ frontend/src/pages/Index.vue
@@ -1,23 +1,83 @@
 <template>
 	<Layout>
-		<h1>This is the home page</h1>
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste
-			tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-		</p>
+        <md-card>
+            <md-card-header>
+                <div class="md-title">Find Classes</div>
+            </md-card-header>
+            <md-divider></md-divider>
+            <md-card-content>
+                <md-tabs class="md-transparent" md-alignment="fixed" md-active-tab="tab-subject">
+                  <md-tab id="tab-subject" md-label="Subject">
+                    <form>
+                        <md-field>
+                            <label>Subject...</label>
+                            <md-input type="text" v-model="subjectForm.name" required />
+                        </md-field>
+                    </form>
+                  </md-tab>
+                  <md-tab id="tab-location" md-label="Location">
+                      <form>
+                        <md-field>
+                            <label>Location...</label>
+                            <md-input type="text" v-model="locationForm.name" required />
+                        </md-field>
+                    </form>
+                  </md-tab>
+                  <md-tab id="tab-time" md-label="Time">
+                      <form>
+                        <md-field>
+                            <label>Time...</label>
+                            <md-input type="text" v-model="timeForm.name" required />
+                        </md-field>
+                    </form>
+                  </md-tab>
+            </md-tabs>
+            </md-card-content>
+            
+            <md-divider></md-divider>
+            <md-card-header>
+                <div class="md-title">Applied Filters</div>
+            </md-card-header>
+            <md-divider></md-divider>
+            <md-card-content>
+            </md-card-content>
+        </md-card>
 	</Layout>
 </template>
 
 <script>
+const initialState = {
+  isLoggedIn: false,
+  currentUser: {
+    name: "",
+    net_id: ""
+  },
+  loginForm: {
+    net_id: "",
+    password: ""
+  },
+  registerForm: {
+    net_id: "",
+    name: "",
+    password: ""
+  }
+};
+
 export default {
-	metaInfo: {
-		title: 'Home'
-	}
+  name: "Home",
+  data() {
+    return {
+        subjectForm: { ...initialState.subjectForm },
+        locationForm: { ...initialState.locationForm },
+        timeForm: { ...initialState.timeForm }
+    };
+  },
+  methods: {
+    
+  }
 };
 </script>
 
 <style>
-.home-links a {
-	margin-right: 1rem;
-}
+
 </style>
